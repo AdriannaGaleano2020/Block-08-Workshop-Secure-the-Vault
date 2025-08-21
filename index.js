fe2cf5e2d5e8e3df086a025d6d16a403bed1003f
Adrianna Galeano <Galeano.adrianna.1990@outlook.com>
2025-08-20 22:40:46 -0400

    Block 8

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("code1");
+console.log("code1", (7+3));
+console.log("code2", 100/2-10);
+console.log("code3", 13%3 +38);
+console.log(message = "The Vault has been secured. The combination is:.");
+let code1 =10;
+let code2 =40;
+let code3 =39;
+console.log( code1+code2+code3);
